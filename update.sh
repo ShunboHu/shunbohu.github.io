4e55c4fb453843bee4a38ac46ccb384f967f76b8
Shunbo Hu <shunbohu@Shunbos-iMac.local>
2021-11-28 16:53:54 +0800

    first commit

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git init
-git add  *.html assets images doc *.sh timetable
+git add  main.html index.html timetable.html assets images doc update.sh timetable
 git commit -m "first commit"
 git branch -M main
 git push -u origin main
