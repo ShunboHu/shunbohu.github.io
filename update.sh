ab50e0a84781dd60bde4c832917456b5ac0b0ecb
ShunboHu <shunbohu@shu.edu.cn>
2022-05-18 22:07:12 +0800

    更新文章列表

--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@
 
 git init
 git add  main.html index.html timetable.html assets images doc update.sh timetable
-git commit -m "first commit"
+git commit -m "更新文章列表"
+git pull
 git branch -M main
 git push -u origin main
