66435610088b95cbdbc07d8c9a7b39c95027a0a8
ShunboHu <shunbohu@shu.edu.cn>
2022-05-18 23:24:14 +0800

    更新网页

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 git init
 git add  main.html index.html timetable.html assets images doc update.sh timetable
-git commit -m "更新文章列表"
+git commit -m "更新网页"
 git pull
 git branch -M main
 git push -u origin main
